d82e427e2cc4d2171b576b91c289fe591dac31db
Farhan Nadim <farhannadim2023@gmail.com>
2025-08-22 12:06:23 +0600

    Configure Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./**/templates/**/*.html",   // For app level
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
