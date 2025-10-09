331ba14ce4e654e5269b1b3c37aa1ed101437337
Farhan Nadim <farhannadim2023@gmail.com>
2025-10-10 00:49:45 +0600

    Implement user management features with country and district associations
    - Added User, Country, and District models with appropriate relationships.
    - Created serializers for User, Country, and District.
    - Developed viewsets for managing users, countries, and districts.
    - Registered models in the admin interface with a custom user admin.
    - Added API endpoints for user and district management.
    - Included fixture data for countries and districts.

--- CarbonLens/users/admin.py
+++ CarbonLens/users/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Country, District
 
-# Register your models here.
+
+class CustomUserAdmin( UserAdmin ):
+    model = User
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'country', 'district')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ("Important Date's", {'fields': ('last_login', 'date_joined')})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'country', 'district', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
+
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active'
+    )
+
+    search_fields = (
+        'username',
+        'first_name',
+        'last_name',
+    )
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Country)
+admin.site.register(District)
--- CarbonLens/users/models.py
+++ CarbonLens/users/models.py
@@ -1,4 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class District(models.Model):
+    name = models.CharField(max_length=200)
+
+    country = models.ForeignKey(
+        Country, 
+        on_delete=models.CASCADE,
+        related_name="district"
+    )
+
+    def __str__(self):
+        return self.name
+    
+
+class User(AbstractUser):
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="users"
+    )
+
+    district = models.ForeignKey(
+        District,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="users"
+    )
+
+    def __str__(self):
+        return self.get_full_name()
