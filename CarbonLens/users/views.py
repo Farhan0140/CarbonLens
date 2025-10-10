a5d282c55b1a2ae4b3bfc9255c9ddc0cdddf36b7
Farhan Nadim <farhannadim2023@gmail.com>
2025-10-10 21:04:02 +0600

    Fixed some bugs in the country and district model

--- CarbonLens/users/views.py
+++ CarbonLens/users/views.py
@@ -1,17 +1,19 @@
 from rest_framework import viewsets
 from .models import Country, District
-from .serializers import (
-    CountrySerializer,
-    DistrictSerializer,
-    DistrictCreateSerializer,
-)
+from .serializers import (CountrySerializer, DistrictSerializer, DistrictCreateSerializer,)
+from core.permissions import IsAdminOrReadOnly
+
+
 
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
 
 class DistrictViewSet(viewsets.ModelViewSet):
     queryset = District.objects.select_related('country').all()
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
