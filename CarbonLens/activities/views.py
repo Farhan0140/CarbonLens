ecf14954d52e3baf04f5e560081a0835634f5ce5
Farhan Nadim <farhannadim2023@gmail.com>
2025-11-13 23:01:35 +0600

    added new end-point

--- CarbonLens/activities/views.py
+++ CarbonLens/activities/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Activity, ActivityRecord
 from .serializers import ActivitySerializer, ActivityRecordSerializer
 from core.permissions import IsAdminOrReadOnly
@@ -8,6 +10,15 @@ class ActivityViewSet(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
     permission_classes = [IsAdminOrReadOnly]  # Only admin can add/edit activities
+
+    @action(detail=False, methods=['get'], url_path='categories', permission_classes=[permissions.AllowAny])
+    def categories(self, request):
+        """Return all available activity categories."""
+        categories = [
+            {"key": key, "label": label}
+            for key, label in Activity.CATEGORY_CHOICES
+        ]
+        return Response(categories)
 
 
 class ActivityRecordViewSet(viewsets.ModelViewSet):
